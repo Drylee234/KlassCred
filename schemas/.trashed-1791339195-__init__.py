1bf966f160a96d0ddeabf91441ba2e7354001010
Drylee234 <ofreddy234@gmail.com>
2026-09-07 03:57:33 +0100

    removed trash files

--- schemas/.trashed-1791339195-__init__.py
+++
@@ -1,8 +0,0 @@
-from .teacher import TeacherSchema
-from .employer import EmployerSchema, RecruitmentHistorySchema
-from .reviewer import ReviewerSchema
-from .exam import (ExamQuestionSchema,ExamSchema,ExamStartSchema,ExamSubmitSchema,ExamAttemptSchema,)
-from .video import TeachingScenarioSchema, VideoSubmissionSchema
-from .review import (ReviewAssignmentSchema,ReviewSchema,HumanReviewSubmitSchema,)
-from .rating import RatingSchema
-from .interview import InterviewRequestSchema
